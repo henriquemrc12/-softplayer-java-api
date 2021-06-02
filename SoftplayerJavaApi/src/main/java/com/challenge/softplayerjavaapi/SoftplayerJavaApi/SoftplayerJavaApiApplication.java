f4ebd9c2d0eeae0c034ab035258f3ece2a39948e
henriquemrc12 <henriquemarcel2244@gmail.com>
2021-06-01 23:17:26 -0300

    Projeto criado

---
+++ SoftplayerJavaApi/src/main/java/com/challenge/softplayerjavaapi/SoftplayerJavaApi/SoftplayerJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.softplayerjavaapi.SoftplayerJavaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftplayerJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftplayerJavaApiApplication.class, args);
+	}
+
+}
